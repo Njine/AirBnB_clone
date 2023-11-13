e61087bc13ce24b2f25e9d92534d89d15994b7dc
drissOubah <drissoubahmane@gmail.com>
2023-11-13 15:42:24 -0800

    City

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Implementation of the City class model"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Contains the City class"""
+    state_id = ""
+    name = ""

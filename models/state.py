01d88c834327f1c8159e3616d90e3418c2579ca9
drissOubah <drissoubahmane@gmail.com>
2023-11-13 15:43:34 -0800

    State

---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Implementation of the State class model"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Implements the State model """
+    name = ""
